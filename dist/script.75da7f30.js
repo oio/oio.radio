0c8a3621dfc0fb9054406a3052d7c3b21dd04b81
matlo <write@matlo.me>
2020-03-25 17:42:33 +0000

    visuals

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -118,7 +118,56 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"script.js":[function(require,module,exports) {
-console.log("jj");
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Initiate gifLoop for set interval
+  var refresh; // Duration count in seconds
+
+  var duration = 1000 * 3; // Giphy API defaults
+
+  var giphy = {
+    baseURL: "https://api.giphy.com/v1/gifs/",
+    apiKey: "0UTRbFtkMxAplrohufYco5IY74U8hOes",
+    tag: "trippy",
+    type: "random",
+    rating: "pg-13"
+  }; // Target gif-wrap container
+
+  var $gif_wrap = document.getElementById("gif-wrap"); // Giphy API URL
+
+  var giphyURL = encodeURI(giphy.baseURL + giphy.type + "?api_key=" + giphy.apiKey + "&tag=" + giphy.tag + "&rating=" + giphy.rating);
+
+  var newGif = function newGif() {
+    fetch(giphyURL).then(function (res) {
+      return res.json();
+    }).then(function (out) {
+      renderGif(out.data);
+    }).catch(function (err) {
+      throw err;
+    });
+  }; // Display Gif in gif wrap container
+
+
+  var renderGif = function renderGif(_giphy) {
+    // Set gif as bg image
+    document.querySelector(".channel").style.backgroundImage = "url('" + _giphy.image_original_url + "')"; // Start duration countdown
+
+    refreshRate();
+  }; // Call for new gif after duration
+
+
+  var refreshRate = function refreshRate() {
+    // Reset set intervals
+    clearInterval(refresh);
+    refresh = setInterval(function () {
+      // Call Giphy API for new gif
+      if (Math.random() > 0.7) newGif();else document.querySelector(".channel").style.backgroundImage = "url('static.gif')";
+    }, duration);
+  }; // Call Giphy API for new gif
+
+
+  newGif(); // const newGifButton = document.getElementById('new-gif');
+  // newGifButton.onclick = newGif
+});
 },{}],"../../../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +196,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51635" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54712" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/script.js
+++ src/script.js
@@ -1,1 +1,66 @@
-console.log("jj")
+document.addEventListener("DOMContentLoaded", function(event) {
+	// Initiate gifLoop for set interval
+	var refresh;
+	// Duration count in seconds
+	const duration = 1000 * 3;
+	// Giphy API defaults
+	const giphy = {
+		baseURL: "https://api.giphy.com/v1/gifs/",
+		apiKey: "0UTRbFtkMxAplrohufYco5IY74U8hOes",
+		tag: "trippy",
+		type: "random",
+		rating: "pg-13"
+	};
+	// Target gif-wrap container
+	const $gif_wrap = document.getElementById("gif-wrap");
+	// Giphy API URL
+	let giphyURL = encodeURI(
+		giphy.baseURL +
+			giphy.type +
+			"?api_key=" +
+			giphy.apiKey +
+			"&tag=" +
+			giphy.tag +
+			"&rating=" +
+			giphy.rating
+	);	
+
+    let newGif = () => {
+        fetch(giphyURL)
+        .then(res => res.json())
+        .then((out) => {
+            renderGif(out.data);        
+            })
+        .catch(err => { throw err });
+        }
+
+	// Display Gif in gif wrap container
+	var renderGif = _giphy => {		
+		// Set gif as bg image
+		document.querySelector(".channel").style.backgroundImage =  "url('" + _giphy.image_original_url + "')"
+
+		// Start duration countdown
+		refreshRate();
+	};
+
+	// Call for new gif after duration
+	var refreshRate = () => {
+		// Reset set intervals
+		clearInterval(refresh);
+		refresh = setInterval(function() {            
+            // Call Giphy API for new gif
+            if (Math.random() > 0.7) 
+                newGif();            
+            else         
+                document.querySelector(".channel").style.backgroundImage =  "url('static.gif')"            
+		}, duration);
+	};
+
+	// Call Giphy API for new gif
+	newGif();
+	
+	
+	// const newGifButton = document.getElementById('new-gif');
+	
+	// newGifButton.onclick = newGif
+});
