e80f8e30e64ac6f673cec3a947d373bd73ec110f
mat-lo <write@matlo.me>
2025-04-16 12:57:03 +0200

    svelte5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+
+	let { children } = $props();
 </script>
 
-<main>
-  <slot />
-</main>
+{@render children()}
--- src/routes/api/getGif/+server.js
+++ src/routes/api/getGif/+server.js
@@ -1,26 +1,26 @@
-import { env } from "$env/dynamic/private";
-import { error } from "@sveltejs/kit";
+import { env } from '$env/dynamic/private';
+import { error } from '@sveltejs/kit';
 
 const key = env.GIPHY_KEY;
 
 // This function fetches a random gif from Giphy based on the given tag
 export async function GET({ url }) {
-  // Extract the tag from the query string
-  const tag = url.searchParams.get("tag") || "hypnotic";
-  const giphyURL = `https://api.giphy.com/v1/gifs/random?api_key=${key}&tag=${tag}&rating=pg-13`;
+	// Extract the tag from the query string
+	const tag = url.searchParams.get('tag') || 'hypnotic';
+	const giphyURL = `https://api.giphy.com/v1/gifs/random?api_key=${key}&tag=${tag}&rating=pg-13`;
 
-  try {
-    const response = await fetch(giphyURL);
-    if (!response.ok) {
-      throw error(500, `Failed to fetch GIF from Giphy`);
-    }
-    const data = await response.json();
-    return new Response(JSON.stringify(data.data.images.original.url), {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (err) {
-    throw error(500, `Error fetching GIF: ${err.message}`);
-  }
+	try {
+		const response = await fetch(giphyURL);
+		if (!response.ok) {
+			throw error(500, `Failed to fetch GIF from Giphy`);
+		}
+		const data = await response.json();
+		return new Response(JSON.stringify(data.data.images.original.url), {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	} catch (err) {
+		throw error(500, `Error fetching GIF: ${err.message}`);
+	}
 }
